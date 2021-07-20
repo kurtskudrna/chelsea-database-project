f0b9386c8251bf9d4223b42b882c14e5c5184dcb
Kurt Skudrna <kurtskudrna@gmail.com>
2021-07-20 11:24:56 -0400

    make migrations, create model and template

--- players/models.py
+++ players/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveSmallIntegerField()
+    description = models.TextField()
+    nationality = models.CharField(max_length=100)
+    image = models.FilePathField(path='/players/img')
