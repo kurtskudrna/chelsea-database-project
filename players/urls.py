410db2a5607154986e9a5e8dbcaa15e64552d609
Kurt Skudrna <kurtskudrna@gmail.com>
2021-07-20 20:04:12 -0400

    finish styling, routing for info button and adding players

--- players/urls.py
+++ players/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from players import views
 
+
+app_name = 'players'
+
 urlpatterns = [
-    path('', views.all_players),
+    path('', views.all_players, name='all_players'),
+    path('<int:pk>', views.player_detail, name='player_detail'),
 ]
--- players/views.py
+++ players/views.py
@@ -6,3 +6,8 @@ from players.models import Player
 def all_players(request):
     players = Player.objects.all()
     return render(request, 'players/all_players.html', {'players': players})
+
+
+def player_detail(request, pk):
+    player = Player.objects.get(pk=pk)
+    return render(request, 'players/detail.html', {'player': player})
